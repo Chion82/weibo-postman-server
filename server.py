e144b93e2d99ec1caba38dd3eef90da18564babc
Chion Tang <sdspeedonion@gmail.com>
2016-03-01 20:54:27 +0800

    Use list to match image

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ from urllib import quote
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+known_ship_ids = [1, 125, 153, 18, 27, 409, 442, 49, 68, 87, 10, 126, 154, 181, 28, 41, 443, 50, 69, 89, 100, 127, 155, 182, 29, 410, 445, 51, 7, 9, 101, 128, 16, 183, 30, 413, 446, 52, 70, 90, 102, 13, 161, 184, 31, 414, 448, 53, 71, 91, 103, 131, 163, 185, 32, 415, 45, 54, 72, 92, 11, 132, 164, 186, 33, 42, 451, 55, 74, 93, 110, 133, 165, 19, 331, 421, 452, 56, 75, 94, 111, 134, 167, 190, 332, 422, 453, 59, 76, 95, 113, 135, 168, 191, 34, 423, 454, 6, 77, 96, 114, 137, 169, 2, 35, 424, 455, 60, 78, 97, 115, 138, 17, 20, 36, 425, 458, 61, 79, 98, 116, 139, 170, 21, 37, 43, 459, 62, 80, 99, 12, 14, 171, 22, 38, 431, 46, 63, 81, 120, 140, 173, 23, 39, 432, 460, 64, 83, 122, 143, 174, 24, 40, 436, 465, 65, 84, 123, 147, 175, 25, 404, 44, 47, 66, 85, 124, 15, 176, 26, 405, 441, 48, 67, 86]
+
 app = Flask(__name__)
 app.secret_key = open('session_key', 'r').read().replace('\n', '')
 
@@ -50,9 +52,8 @@ def api_post_weibo():
 		api_ship_id = request.args['api_ship_id']
 
 	post_image_flag = False
-	if api_ship_id != None and ship_get_image_exists(api_ship_id):
-		if (ship_get_image_exists(api_ship_id)):
-			post_image_flag = True
+	if api_ship_id != None and int(api_ship_id) in known_ship_ids:
+		post_image_flag = True
 
 	if post_image_flag:
 		request_result = requests.post('https://api.weibo.com/2/statuses/upload_url_text.json', {
@@ -79,9 +80,6 @@ def is_access_token_valid(access_token):
 		return True
 	else:
 		return False
-
-def ship_get_image_exists(api_ship_id):
-	return os.path.isfile('ship_images/' + api_ship_id + '.png')
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5011, debug=True)
